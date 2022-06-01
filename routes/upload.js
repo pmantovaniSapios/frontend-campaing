da5c4cd06e058b21695961770a3a50177192d005
Pedro Mantovani <pmantovani@sapios.com.br>
2022-06-01 13:42:35 -0300

    ajuste

--- routes/upload.js
+++ routes/upload.js
@@ -10,9 +10,9 @@ router.post('/upload', upload.single("file"), async function(req, res, next) {
   let retorno = await csvToJson(req.file.filename);
 
   if (retorno == true) {
-    res.render("upload");
+    res.render("upload", { title: "Dashboard - Campanha Sapios" });
   } else {
-    res.render("upload");
+    res.render("upload", { title: "Error - Campanha Sapios" });
   }
 
 });
