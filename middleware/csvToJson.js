f6736691fe402f19fe820af87f708c4b9819e19f
Pedro Mantovani <pmantovani@sapios.com.br>
2022-06-06 16:23:59 -0300

    ajuste middleware

--- middleware/csvToJson.js
+++ middleware/csvToJson.js
@@ -37,57 +37,6 @@ async function csvToJson(filepath) {
       csvData.push(data);
     })
     .on("end", async () => {
-      // for (let index = 0; index < csvData.length; index++) {
-      //     conn.query(`INSERT INTO datacampaings SET
-      //                 campaingsId=1,
-      //                 nome="${csvData[index].nome}",
-      //                 phone="${csvData[index].phone}",
-      //                 field01="${csvData[index].field01}",
-      //                 field02="${csvData[index].field02}",
-      //                 field03="${csvData[index].field03}",
-      //                 field04="${csvData[index].field04}",
-      //                 field05="${csvData[index].field05}",
-      //                 field06="${csvData[index].field06}",
-      //                 field07="${csvData[index].field07}",
-      //                 field08="${csvData[index].field08}",
-      //                 field09="${csvData[index].field09}",
-      //                 field10="${csvData[index].field10}",
-      //                 field11="${csvData[index].field11}",
-      //                 field12="${csvData[index].field12}",
-      //                 field13="${csvData[index].field13}",
-      //                 field14="${csvData[index].field14}",
-      //                 field15="${csvData[index].field15}",
-      //                 field16="${csvData[index].field16}",
-      //                 field17="${csvData[index].field17}",
-      //                 field18="${csvData[index].field18}",
-      //                 field19="${csvData[index].field19}",
-      //                 field20="${csvData[index].field20}"
-      //                     ON DUPLICATE KEY UPDATE
-      //                         nome="${csvData[index].nome}",
-      //                         phone="${csvData[index].phone}",
-      //                         field01="${csvData[index].field01}",
-      //                         field02="${csvData[index].field02}",
-      //                         field03="${csvData[index].field03}",
-      //                         field04="${csvData[index].field04}",
-      //                         field05="${csvData[index].field05}",
-      //                         field06="${csvData[index].field06}",
-      //                         field07="${csvData[index].field07}",
-      //                         field08="${csvData[index].field08}",
-      //                         field09="${csvData[index].field09}",
-      //                         field10="${csvData[index].field10}",
-      //                         field11="${csvData[index].field11}",
-      //                         field12="${csvData[index].field12}",
-      //                         field13="${csvData[index].field13}",
-      //                         field14="${csvData[index].field14}",
-      //                         field15="${csvData[index].field15}",
-      //                         field16="${csvData[index].field16}",
-      //                         field17="${csvData[index].field17}",
-      //                         field18="${csvData[index].field18}",
-      //                         field19="${csvData[index].field19}",
-      //                         field20="${csvData[index].field20}"
-
-      console.log("end");
-
       axios({
         url: "https://connector.sapios.com.br/hap/datafromcsv",
         method: "post",
@@ -95,7 +44,7 @@ async function csvToJson(filepath) {
       })
         .then(function (response) {
           // your action after success
-          console.log(response);
+          // console.log(response);
         })
         .catch(function (error) {
           // your action on error success
